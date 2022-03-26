2239564c5c9e5cef234ff6e294e7b46015edc564
alexeyprimechaev <51163708+alexeyprimechaev@users.noreply.github.com>
2022-03-27 00:47:11 +0300

    Initial Commit

---
+++ Starlight/StarlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarlightApp.swift
+//  Starlight
+//
+//  Created by Alexey Primechaev on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct StarlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
